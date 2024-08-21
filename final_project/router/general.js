88878b25b65d1bc2305579024a9483d544e68e58
rrehus <rrehus98@gmail.com>
2024-08-21 03:31:19 -0400

    Task 10-13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -39,21 +39,6 @@ public_users.get('/',function (req, res) {
     return res.send(JSON.stringify(books,null));
   });
 
-
-
-const connectToURL = async (url) => {
-    try {
-        const response = await axios.get(url);
-        console.log(response.data);
-    } catch (error) {
-        console.error(error.toString());
-    }
-}
-
-connectToURL('https://ernore66-5000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/');
-
-
-
 public_users.get('/users',function (req, res) {
     //Write your code here
     return res.send(users);
@@ -93,5 +78,60 @@ public_users.get('/review/:isbn',function (req, res) {
     let reviews = books[isbn].reviews;
     return res.send(reviews);
 });
+
+//Task 10
+const getBookList = async (url) => {
+    try {
+        const response = await axios.get(url);
+        console.log(response.data);
+    } catch (error) {
+        console.error(error.toString());
+    }
+}
+
+//getBookList('http://localhost:5000/');
+
+// Task 11
+
+const getBookDetailsBasedonISBN = (url)=>{
+    const req = axios.get(url);
+    req.then(resp => {
+        let data = resp.data;
+        console.log(data);
+      })
+    .catch(err => {
+        console.log(err.toString())
+    });
+  }
+
+//getBookDetailsBasedonISBN('http://localhost:5000/isbn/1')
+
+// Task 12
+
+const getBookDetailsBasedonAuthor = async (url) => {
+    try {
+        const response = await axios.get(url);
+        console.log(response.data);
+    } catch (error) {
+        console.error(error.toString());
+    }
+}
+
+//getBookDetailsBasedonAuthor('http://localhost:5000/author/Chinua Achebe')
+
+//Task 13
+
+
+const getBookDetailsBasedonTitle = async (url) => {
+    try {
+        const response = await axios.get(url);
+        console.log(response.data);
+    } catch (error) {
+        console.error(error.toString());
+    }
+}
+
+getBookDetailsBasedonTitle('http://localhost:5000/title/Things Fall Apart')
+
 
 module.exports.general = public_users;
